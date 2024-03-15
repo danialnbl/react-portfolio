a21e81e1cdc169b6ed1302e98664cf8a8ecc2b6a
danialnbl <danialnabil0208@gmail.com>
2024-03-15 13:10:47 +0800

    update resume

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="CV_DanialNabil_01123138061.pdf"
+                  href="Resume_DanialNabil_01123138061.pdf"
                 >
                   Resume
                 </a>
